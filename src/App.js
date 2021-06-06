9b5891781ba284c63665ee732660d682e7039e48
wissem <wissemabidh@gmail.com>
2021-06-06 20:47:45 +0200

    upload

--- src/App.js
+++ src/App.js
@@ -1,151 +1,51 @@
-import logo from './logo.svg';
-import React,{useState} from 'react'
-import './App.css';
-import Login from './Compent/Login/login'
-import Register from './Compent/Register/Register'
-import {BrowserRouter ,Route, Switch} from 'react-router-dom'
-import Home from './Compent/Home/Home'
-import Catgory from './Compent/Catgory/Catgory'
-import Producdt from './Compent/Home/Product03'
-import Confirm from './Compent/Confirm/Confirm'
-import User from './Compent/userInterface/User'
-import User02 from './userInterface02/User02'
-import Profile from './userInterface02/Profile'
-import ResetPassword from '../src/Compent/Login/ResetPassword'
-import "aos/dist/aos.css"
-import PriverRoute02 from './Compent/routes/PrivateRoutes'
-import Test01 from './Test'
-import Register02 from './Compent/login02/login'
-import Register4 from './Compent/login02/Register02Check4'
-import Check from './Compent/routes/Check'
-import ProfileUpdate from './userInterface02/ProfileUpdate'
-import ProgresBar from './userInterface02/ProgresBar'
+
+
+// Import Css
+import React ,{useState} from'react'
+import './Css/heaight/Heaight.css'
+import './Css/Widht/Widht.css'
+import './Css/BackGroundImageurl/BackgroundImageurl.css'
+import './Css/margin/margin.css'
+import './Css/Resposive/Register.css'
+import './Css/Animation/ProgressBar.css'
+import './App.css'
+import './Css/padding/padding.css'
+import './Css/Resposive/HomePage.css'
+import './Css/Animation/AnimationLogo.css'
+import Home from './Compent/Home/Home.jsx'
+// Import Router 
+import {Route , BrowserRouter } from 'react-router-dom'
+//Compe Imported 
+import Register from './Compent/Register/Register.jsx'
+
 function App() {
-  // Function
-    // 1 userName 
-  const [Bloena  , setBolen ]=useState(false)
-  const [Bloena02  , setBolen02 ]=useState(false)
-  const [Bloena03  , setBolen03 ]=useState(false)
-  const [LengthBolen  , setLengthBolen ]=useState(false)
-  const [Step2  , setStep2 ]=useState(false)
-  const [Step3  , setStep3 ]=useState(false)
-  const [Verf0action  , setVerfaction ]=useState(0)
-  const ConfirmEmail = JSON.parse(localStorage.getItem('Token')) 
-  const Test =(UserName,Email , ConfirmEmail , password )=>{
-    if(UserName && UserName.length>6 && Email && ConfirmEmail && Email===ConfirmEmail &&Email.includes('@')&&ConfirmEmail.includes('@')  ){
-      console.log('dqzdq')
-      setBolen(true)
-      console.log(Bloena)
-    }else{
-      console.log("le")
-    }
-  }
-  const Test03 =(Nom , Pernom ,Email , Modpaas , confirmModpass )=>{
+  // Function 
+  // Registe 1 
+  const [RedirectRegister , setRedirectRegister]=useState(false)
+  const [RedirectRegister2 , setRedirectRegister2]=useState(false)
+  const RegisterFunction1 =(Nom , Pernom ,Email , Modpaas , confirmModpass )=>{
     if(Nom && Pernom && Email && Modpaas && Email.includes('@') && Modpaas===confirmModpass &&  Modpaas.length>=6){
-      setBolen(true)
+      setRedirectRegister(true)
     
   }
-}
-const Test04 =(NomCommunauté , Secture ,siret ,  nomentreprizde )=>{
-  if(NomCommunauté&&Secture&&siret&&nomentreprizde){
-    setBolen02(true)
   }
-
-}
-const Prevlise =( )=>{
-
-    setBolen02(false)
-  
-
-}
-const Test05 =(Sexe , Date  ,Adress , Ville , Pays )=>{
-  if(Sexe&&Date&&Adress&&Ville&&Pays){
-    setBolen03(true)
+  // Register02 
+  const RegisterFunction2 =(Sectur , Comunites,Siret ,Nomentreprise )=>{
+    if(Sectur&&Comunites&&Siret&&Nomentreprise){
+      setRedirectRegister2(true)
+    
   }
-  console.log(Bloena03)
-}
-
-  // Setp2 
- 
-  const Step2d =(Communite,LederName , Date ,Sex)=>{
-    if(Communite || LederName && Date && Sex  ){
-      console.log('dqzdq')
-      setStep2(true)
-      console.log(Step2)
-    }else{
-      console.log("le")
-    }
   }
-  //setp3
-  const Step3d =(LederNamed,Test  )=>{
-    if(LederNamed && Test && LederNamed===Test && LederNamed.length>=5 ){
-      console.log('dqzdq')
-      setStep3(true)
-      console.log(Step3)
-    }else{
-      console.log("le")
-    }
-  }
-
-  // Verfaction System
-  const Verfaction1 =(Usersdname , Email )=>{
-    if(Usersdname||Email)  {
-      setVerfaction(15)
-    }
-    if(Usersdname&&Email) {
-      setVerfaction(30)
-    }
-  }
-  const Verfaction02 =(role , comunite , Datadenasisence , sex )=>{
-    if(role||comunite)  {
-      setVerfaction(Verf0action+30)
-    }
-    if(Datadenasisence||sex)  {
-      setVerfaction(Verf0action+30)
-    }
-    // Verf0action
-    if(role&&comunite&&Datadenasisence&&sex) {
-      setVerfaction(75)
-    }
-  } 
-  
   return (
-    
-    <div  className="App">
-       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous"></link>
+    <div className="App">
       <BrowserRouter>
-      <Route exact path="/login" render={()=><Login/>}></Route>
-      <Route exact path="/register" render={()=><Register Verfaction02={Verfaction02} Verf0action={Verf0action} Verfaction1={Verfaction1} Test={Test} Bloena={Bloena} Step2d={Step2d} Step2={Step2} Step3d={Step3d} Step3={Step3} setBolen02={setBolen02} />}></Route>
-      {/* <Route exact path="/register/1" render={()=><Register Test={Test} Bloena={Bloena} />}></Route> */}
-    
-      <Route  exact path="/" render={()=> <Home/> } /> 
-    
-      {console.log(ConfirmEmail)}
-      <Route exact path="/Catgory" render={()=><Catgory/>} /> 
-      <Route exact path="/Product" render={()=><Catgory/>} />
-      <Route exact path="/Product01" render={()=><Producdt/>} />
-      <Route exact path="/password" render={()=><ResetPassword/>} />
-      <Route exact path="/Test" render={()=><Test01/>} />
-      {ConfirmEmail&&    <Route exact path="/Confirm" render={()=><Confirm/>} />}
-      {/* User */}
+      {/* Register */}
+      <Route exact path="/Register" render={()=><Register setRedirectRegister2={setRedirectRegister2} setRedirectRegister={setRedirectRegister} RedirectRegister={RedirectRegister} RegisterFunction1={RegisterFunction1} RegisterFunction2={RegisterFunction2} RedirectRegister2={RedirectRegister2}/> } />
+      {/* HomePage */}
+      <Route exact path="/" render={()=>  <Home/>}></Route>
      
-      {/* Producdt */}
-      {/* Confirm */} <Route  path="/Dashboard01/profile" component={User}/>
-                 {/* <PriverRoute02 exact path="/Dashboard01" component={User02}></PriverRoute02>   */}
-                 <Route exact path="/Dashboard01" render={()=><User02/>} /> 
-                
-                 <Route exact path="/Dashboard01/boutique" render={()=><ProfileUpdate/>} /> 
-                 {/* <Route exact path="/Dashboard01/Profie" render={()=><ProgresBar/>} /> */}
-                      <Route  path="/Profile" component={Profile}/>
-                      
-      <Route exact path="/Confirm" render={()=><Confirm/>} />
-      <Route exact path="/Confirm" render={()=><Confirm/>} />
-      <Route exact path="/Login02" render={()=><Register02  Prevlise={Prevlise} setBolen={setBolen} Test05={Test05} Bloena02={Bloena02} Test04={Test04} setLengthBolen={setLengthBolen} LengthBolen={LengthBolen}  Bloena={Bloena} Test03={Test03}/>}/>
-      <Route exact path="/Favoire" render={()=><Register4/>}/>
-      <Route exact path="/Check/:token" render={({match})=><Check match={match.params.token}/>}/>
-      
       </BrowserRouter>
-      
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,41 +1,16 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
-// import  "bootstrap/dist/css/bootstrap.min.css";
-// import {Provider} from 'react-redux'
-// import store from './js/Store/Store'
-// ReactDOM.render(
-// ReactDOM.render(
-//   <Provider store={store}>  
-//     <App />
-//     </Provider>
-//     ,
-
-//   document.getElementById('root')
-// );
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
-import React,{Suspense} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import  "bootstrap/dist/css/bootstrap.min.css";
 import  "bootstrap/dist/css/bootstrap.min.css";
 import {Provider} from 'react-redux'
 import store from './js/Store/Store'
-import './i18next'
 ReactDOM.render(
-  <Suspense fallback={(<div>Loadin --</div>)}>
-  <Provider store={store}>
-        <App />
-      </Provider>
-      </Suspense>
- ,
+<Provider store={store}>
+    <App />
+    </Provider>,
   document.getElementById('root')
 );
 
